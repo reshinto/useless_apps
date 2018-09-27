068e00f18c8d558793d192016ef70f977458c66c
Terence Kong <terence_kong2002@hotmail.com>
2018-09-27 22:54:13 +0900

    add adapter method example for implementation

--- chk_os.py
+++ chk_os.py
@@ -11,3 +11,42 @@ elif current_os == "Windows":
     print("Windows")
 else:
     print("Unknown OS")
+
+
+class Korean:
+    def __init__(self):
+        self.name = "Korean"
+
+    def speak_korean(self):
+        return "An-neyong?"
+
+
+class British:
+    def __init__(self):
+        self.name = "British"
+
+    def speak_english(self):
+        return "Hello!"
+
+
+class Adapter:
+    def __init__(self, object, **adapted_method):
+        self._object = object
+
+        self.__dict__.update(adapted_method)
+
+    def __getattr__(self, attr):
+        return getattr(self._object, attr)
+
+
+objects = []
+
+korean = Korean()
+
+british = British()
+
+objects.append(Adapter(korean, speak=korean.speak_korean))
+objects.append(Adapter(british, speak=british.speak_english))
+
+for obj in objects:
+    print(f"{obj.name} says '{obj.speak()}'\n")
