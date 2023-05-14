1a5574883c60ad20e9a50ee1c91067b58f0d6e4f
Terence <35042213+reshinto@users.noreply.github.com>
2023-05-14 15:34:53 +0800

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # make file executable by running this command in the terminal: chmod + x clone.sh
+# clone at current directory and cd into cloned project -> clone git@github.com:reshinto/myrepo.git
+# clone at target directory and cd into cloned project -> clone git@github.com:reshinto/myrepo.git ./some/path
 
 mkdir -p $2
 cd $2
